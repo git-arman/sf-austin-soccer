a2f5777b33a77fa904a3b99005d8d53b488cb262
arman_shah <ArmanShah70@yahoo.com>
2019-02-21 22:57:25 -0600

    Renamed function to onFormSubmitted for Parent FacilitySearch.cmp

--- src/aura/FacilitySearch/FacilitySearchController.js
+++ src/aura/FacilitySearch/FacilitySearchController.js
@@ -1,9 +1,12 @@
+
+// FacilitySearch CONTROLLER code
+
 ({
-	onFormSubmit : function(component, event, helper) {
+	onFormSubmitted : function(component, event, helper) {
         var formData = event.getParam("formData");
         var facilityTypeId = formData.facilityTypeId;
-        //alert("onFormSubmit: facilityTypeId = "+facilityTypeId);
-      	
+        alert("onFormSubmit: facilityTypeId = "+facilityTypeId);
+
       	var childCmp = component.find("FacilitySearchResults");
         //var auraMethodResult = childCmp.search(facilityTypeId);
         childCmp.search(facilityTypeId);
