0098197d0210344168f8c48cff9336834dd2102f
arman_shah <ArmanShah70@yahoo.com>
2019-03-04 19:30:47 -0600

    Minor: Comment out Alerts

--- src/aura/FacilityDetails/FacilityDetailsController.js
+++ src/aura/FacilityDetails/FacilityDetailsController.js
@@ -1,10 +1,2 @@
 ({
-	/*onFacilitySelected : function(component, event, helper) {
-		alert('IN onFacilitySelected');
-        var facility = event.getParam('facility');
-        alert('onFacilitySelected: facility = '+facility.name);
-        if (facility) {
-            component.set("v.facility", facility);
-        }
-	},*/
 })
--- src/aura/FacilitySearch/FacilitySearchController.js
+++ src/aura/FacilitySearch/FacilitySearchController.js
@@ -1,14 +1,11 @@
-
-// FacilitySearch CONTROLLER code
-
+// FacilitySearch (PARENT) CONTROLLER code
 ({
 	onFormSubmitted : function(component, event, helper) {
         var formData = event.getParam("formData");
         var facilityTypeId = formData.facilityTypeId;
-        alert("onFormSubmit: facilityTypeId = "+facilityTypeId);
+        //alert("PARENT onFormSubmitted: facilityTypeId = "+facilityTypeId);
 
       	var childCmp = component.find("FacilitySearchResults");
-        //var auraMethodResult = childCmp.search(facilityTypeId);
         childCmp.search(facilityTypeId);
 	},
 })
